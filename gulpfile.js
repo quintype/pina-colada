72f8977a15e0a79b307bcc01c4baf0481c187de5
Tejas Dinkar <tejas@gja.in>
2016-08-23 08:32:55 +0530

    Browserify also works

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,55 @@
-var gulp = require("gulp");
-var sass = require("gulp-sass");
+var gulp = require('gulp');
+var sass = require('gulp-sass');
 var rev = require('gulp-rev');
-var multistream = require("multistream");
-var util = require("gulp-util");
-var minify = require("gulp-minify");
-var sourcemaps = require("gulp-sourcemaps");
-
-var production = !!util.env.production;
-
-var elixir = require('laravel-elixir');
-var shell = require("gulp-shell")
-var del = require("del");
+var revReplace = require('gulp-rev-replace');
+var merge = require('merge-stream');
+var util = require('gulp-util');
+var minify = require('gulp-minify');
+var sourcemaps = require('gulp-sourcemaps');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var del = require('del');
 
 var destination = "public/pina-colada/assets";
-var tempPath = "tmp/asset";
 
-elixir.config.publicPath = destination;
-
-gulp.task("delete-public", function() {
-  return del([destination]);
-});
-
-gulp.task("compile", function() {
+function compileAll() {
+  var production = !!util.env.production;
   var css = gulp.src("./resources/assets/sass/application.scss")
                 .pipe(production ? util.noop() : sourcemaps.init())
                 .pipe(sass({
                   outputStyle: production ? 'compressed' : 'nested'
                  }).on("error", sass.logError))
                 .pipe(production ? util.noop() : sourcemaps.write());
+
   var images = gulp.src("./resources/assets/images/**/*");
-  return css
+
+  var js = browserify({
+      entries: "./resources/assets/js/application.js"
+    }).bundle()
+    .pipe(source("application.js"))
+    .pipe(buffer())
+    .pipe(production ? util.noop() : sourcemaps.init())
+    .pipe(production ? uglify() : util.noop())
+    .pipe(production ? util.noop() : sourcemaps.write());
+
+  return merge(css, js, images)
            .pipe(rev())
-           .pipe(gulp.dest("./foo"))
+           .pipe(revReplace())
+           .pipe(gulp.dest(destination))
            .pipe(rev.manifest())
-           .pipe(gulp.dest("./resources"));
+           .pipe(gulp.dest(destination));
+};
+
+gulp.task("clean", function() {
+  return del([destination]);
 });
 
-elixir(function(mix) {
-    mix.task("delete-public");
-});
+gulp.task("compile", compileAll);
+gulp.task("clean-compile", ["clean"], compileAll);
+gulp.task("default", ["clean-compile"]);
 
-elixir(function(mix) {
-    mix.sass('application.scss', tempPath + "/application.css");
+gulp.task('watch', ['clean-compile'], function() {
+  gulp.watch("./resources/assets/**/*", ['compile']);
 });
-
-elixir(function(mix) {
-    mix.browserify("application.js", tempPath + "/application.js");
-});
-
-elixir(function(mix) {
-    mix.copy("resources/assets/images", tempPath);
-});
-
-elixir(function(mix) {
-    mix.copy(tempPath, destination);
-});
-
-elixir(function(mix) {
-    mix.version([
-      "application.js",
-      "application.css",
-      "**/*.gif",
-      "**/*.jpg",
-      "**/*.png",
-      "**/*.svg"
-    ], destination);
-});
-
