8263330f385c7b6a39383b927cb2dab3d9a081b0
Tejas Dinkar <tejas@gja.in>
2016-08-23 06:44:49 +0530

    Production Quality SCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,13 @@
 var gulp = require("gulp");
+var sass = require("gulp-sass");
+var rev = require('gulp-rev');
+var multistream = require("multistream");
+var util = require("gulp-util");
+var minify = require("gulp-minify");
+var sourcemaps = require("gulp-sourcemaps");
+
+var production = !!util.env.production;
+
 var elixir = require('laravel-elixir');
 var shell = require("gulp-shell")
 var del = require("del");
@@ -10,6 +19,21 @@ elixir.config.publicPath = destination;
 
 gulp.task("delete-public", function() {
   return del([destination]);
+});
+
+gulp.task("compile", function() {
+  var css = gulp.src("./resources/assets/sass/application.scss")
+                .pipe(production ? util.noop() : sourcemaps.init())
+                .pipe(sass({
+                  outputStyle: production ? 'compressed' : 'nested'
+                 }).on("error", sass.logError))
+                .pipe(production ? util.noop() : sourcemaps.write());
+  var images = gulp.src("./resources/assets/images/**/*");
+  return css
+           .pipe(rev())
+           .pipe(gulp.dest("./foo"))
+           .pipe(rev.manifest())
+           .pipe(gulp.dest("./resources"));
 });
 
 elixir(function(mix) {
