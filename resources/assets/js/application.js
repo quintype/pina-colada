74f37bc6079faba98db4bace3cd7057b8caa06d1
Kavya <kavya@quintype.com>
2016-10-07 15:45:16 +0530

    adding videoJS file for you tube elements

--- resources/assets/js/application.js
+++ resources/assets/js/application.js
@@ -1,5 +1,13 @@
 global.app = {
   initQtReady: require("./qt_ready"),
   startHomePreview: require("./home_preview"),
-  quintypeLoadMore : require("./load_more")
+  quintypeLoadMore : require("./load_more"),
+  video: require("./video"),
+  locationOverlay: require("./location_overlay")
 };
+
+$(document).ready(function() {
+  app.video.setupYoutubeVideo();
+  app.video.loadYoutubeLibrary();
+  app.locationOverlay.locationOverlayForMaps();
+});
