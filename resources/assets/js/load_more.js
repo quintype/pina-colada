622050046343a19f82a882b506b0f39a14684081
Anagh Padmanabhan <anagh.parambath@gmail.com>
2016-11-10 21:10:13 +0530

    Refactored

--- resources/assets/js/load_more.js
+++ resources/assets/js/load_more.js
@@ -1,27 +1,23 @@
 var _ = require("lodash");
 
-function quintypeLoadMore (container, params, offset, api, storyTemplate) {
-  var moreStoriesTemplate ,stories;
-  var api_search='/api/v1/search?';
-  moreStoriesTemplate = require("./templates");
-  moreStoriesTemplate = moreStoriesTemplate[storyTemplate];
+function quintypeLoadMore (container, params, api, storyTemplate) {
+  var api_search = '/api/v1/search?';
+  var moreStoriesTemplate = require("./templates")[storyTemplate];
   var loadButton = container.find('.load-more-button');
   var loadImage = container.find('.load-image');
   params = _.extend({limit: 20}, params);
   container.find(".load-more-button").click(function() {
     loadImage.show();
-    $.get(api, _.extend(params, {offset: offset}), function(response){
-      if (api.indexOf(api_search) !=-1){ //check for search api
-        stories=response.results.stories;
-      }
-      else {
-        stories = response.stories;
+    $.get(api, _.extend(params, {offset: params.offset}), function(response){
+      if (api.indexOf(api_search) != -1){ //check for search api
+        var stories = response.results.stories;
+      } else {
+        var stories = response.stories;
       }
       if(stories.length > 0) {
-        offset = offset + params.limit;
+        params.offset += params.limit;
         container.find(".load-more-results").append(moreStoriesTemplate.render({stories: stories}));
-      }
-      else{
+      } else{
         loadButton.hide();
       }
       loadImage.hide();
