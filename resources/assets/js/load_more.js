0219d93f9738767852e53ab0f32c792de83688d4
Anagh Padmanabhan <anagh.parambath@gmail.com>
2016-11-11 09:48:30 +0530

    Show/hide load more button on click.

--- resources/assets/js/load_more.js
+++ resources/assets/js/load_more.js
@@ -9,16 +9,22 @@ function quintypeLoadMore (container, params, api, storyTemplate) {
   container.find("#load-more-button").click(function() {
     loadButton.hide();
     loadImage.show();
-    $.get(api, _.extend(params, {offset: params.offset}), function(response){
-      if (api.indexOf(api_search) != -1){ //check for search api
+    //Each time, fetch 1 story extra than the required limit so that we can use this to identify
+    //whether or not to show the load more button. Once this is done, restore the original value.
+    //Also, remove(slice) the extra story from being sent over to the template.
+    $.get(api, _.extend(params, { offset: params.offset, limit: params.limit + 1 }), function(response){
+      params.limit--;//Restoring the original value.
+      if (api.indexOf(api_search) != -1){ //check for search api.
         var stories = response.results.stories;
       } else {
         var stories = response.stories;
       }
       if(stories.length > 0) {
         params.offset += params.limit;
-        container.find(".load-more-results").append(moreStoriesTemplate.render({stories: stories}));
-        loadButton.show();
+        container.find(".load-more-results").append(moreStoriesTemplate.render({stories: _.slice(stories, 0, params.limit)}));//Slice the extra story.
+        if(stories.length > params.limit){
+          loadButton.show();
+        }
       } else{
         loadButton.hide();
       }
